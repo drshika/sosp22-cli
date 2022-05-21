39382a963b69be38fa25e1eecba26bb705b28f2f
Drshika Asher <67125579+drshika@users.noreply.github.com>
2022-05-20 23:52:45 -0500

    step 3

--- writing.py
+++ writing.py
@@ -1,9 +1,15 @@
-from plumbum import cli 
+from pyfiglet import Figlet
+from plumbum import colors, cli 
+
+def print_banner(text):
+    with colors['LIGHT_SEA_GREEN']:
+        print(Figlet(font='slant').renderText(text))
 
 class GJournal(cli.Application):
     VERSION = "0.0"
 
     def main(self):
-        print("Hello World")
+        print_banner("Gratitude Journal")
+
 if __name__ == "__main__":
     GJournal()
