afe999773c3b0873bfe583b5b5922056b2843462
Drshika Asher <67125579+drshika@users.noreply.github.com>
2022-05-22 22:33:44 -0500

    feat: add progress bar

--- README.md
+++ README.md
@@ -1,6 +1,73 @@
-# SAIL-cli-gratitude-journal
-Demo Repository for Illinois CS SAIL 2022 Workshop on creating a CLI Gratitude Journal &lt;3.  
-Tutorial: https://drshika.github.io/SAIL-cli-gratitude-journal/
+# gjournal: Command Line Gratitude Journal
 
-Finished Product:
-<img width="810" alt="Screen Shot 2022-03-20 at 2 13 36 PM" src="https://user-images.githubusercontent.com/67125579/159178858-4692413e-ba4b-45d4-b223-58308fcd6179.png">
+Command-line Gratitude Journal with fancy interface features! See the code at `journal.py`.
+
+![shows version and help](version_and_help.png)
+
+![has a nice selector for picking files](select_files.png)
+
+![shows a progress bar as data is being pushed](push_in_progress.png)
+
+Expanded from a [tutorial workshop](https://125summer.tech/cli) I did in Summer 2021 as part of UIUC Summer of Side Projects.
+
+Note that this is an example project to demonstrate the wide variety of command-line interface building libraries that python has available. Of course, an actual client will account for more edge cases and a more extensive feature set.
+
+## Libraries demonstrated
+
+Most of what's being done here with excellent Python libraries:
+
+### questionary
+
+[Questionary](https://questionary.readthedocs.io/en/stable/) does a great job at all sorts of command line question asking. Checkboxes, autocomplete, validations, formatting ad much more. Here we're using it to present the list of files to select as checkboxes.
+
+### pyfiglet
+
+[https://github.com/pwaller/pyfiglet](pyfiglet) is a port of `figlet` for setting up banners with ASCII art. We show the git fancy add banner with it.
+
+### plumbum
+
+[plumbum](https://plumbum.readthedocs.io/en/latest/) does a lot of the heavy lifting in many python command-line based applications. 
+
+With `plumbum.cmd`, we can import bash scripts and call them as python functions. For example, here, we're using it to call `ls` to get the list of current files and `git` to execute our commands on it.
+
+The `cli` module from `plumbum` also lets us use a nice class-based syntax to set a version number, flags, and help text that wrap around our script. This allows us to focus on the content of the program itself.
+
+The article in the See More section is a good place to learn more about `plumbum`.
+
+### rich
+
+Another common library I've seen in python CLI applications is `rich`. It's really good at all sorts of text-based formatting, colors, tables, logs, progress bars, and UTF-8 support. `fgit` is using it for color and text style formatting, as well as displaying the progress bar at the end. 
+
+## See More
+
+* [Building Beautiful Command-Line Interfaces with Python](https://codeburst.io/building-beautiful-command-line-interfaces-with-python-26c7e1bb54df) - Summary of many interesting libraries
+
+* [Plumbum scripting](https://iscinumpy.gitlab.io/post/plumbum-scripting/) - Deeper dive into plumbum for bash-like scripting in python. Pipes, background execution, colors, decorators
+
+## Install
+
+After cloning the repository
+
+1. Have a recent version of `python3` and `pip` installed. 
+
+2. Install from the requirements file
+   ```bash 
+   $ pip install -r requirements.txt
+   ```
+
+## Run
+
+Use `python3` to just run it once the dependencies are set up. 
+
+```bash
+$ python fgit.py [OPTIONS]
+```
+
+If you like, you can make it an executable, add it to your path and indicate the language with a #! hash-bang to make the initial start command smaller.*
+
+### Options 
+
+* `-h`/`--help` - Get help text
+* `-v`/`--version` - Get version 
+* `-c`/`--commit` - Commit files after adding
+* `-p`/`--push` - Push files at the end 
--- journal.py
+++ journal.py
@@ -1,79 +1,100 @@
-from datetime import datetime
 import os, fnmatch
-import textwrap
+from datetime import datetime
+from pyfiglet import Figlet
+from plumbum import colors, cli 
 import questionary
 from questionary import prompt
-from pyfiglet import Figlet
-from plumbum import colors, cli
-from plumbum.cmd import git
 import yaml, ruamel.yaml
+from rich.progress import Progress
+from plumbum.cmd import git
+import textwrap
+import time
 
-with open("config.yml", "r") as yamlfile:
-    data = yaml.safe_load(yamlfile)
-author = data['author']
-path = data['path']
+def print_banner(text):
+    with colors['LIGHT_SEA_GREEN']:
+        print(Figlet(font='slant').renderText(text))
+
+author = ""
+journal_name = ""
+
+# loads a journal yaml config file, creating it if missing
+def load_config(filename):
+    global author, journal_name
+    if not os.path.exists(filename):
+        save_config(filename, {
+            "author": '',
+            "journal_name": ''
+        })
+
+    with open(filename, "r") as file:
+        data = yaml.safe_load(file)
+    author = data['author']
+    journal_name = data['journal_name']
+
+# save the config to a file
+def save_config(filename, config):
+    yaml = ruamel.yaml.YAML()
+
+    with open(filename, "w") as file:
+            yaml.dump(config, file)
+
+def init_folder(folder_name):
+    #making the directory
+    try:
+        os.makedirs(folder_name)
+    except OSError:
+        print(f"Creating the directory {folder_name} has failed.")
+
+    os.chdir(journal_name)
+    add_page()
+
 #(1) buy the journal
 def create_journal():
     #prompt user to name journal
-    global author, path
+    global author, journal_name
     author = ruamel.yaml.scalarstring.DoubleQuotedScalarString(questionary.text("What is your name?").ask())
     
     #find the place to save the journal
-    path = ruamel.yaml.scalarstring.DoubleQuotedScalarString(questionary.text("What is the name of your journal?").ask())
+    journal_name = ruamel.yaml.scalarstring.DoubleQuotedScalarString(author + "-Journal")
 
-    my_dict = dict(author=author, path=path)
-    yaml = ruamel.yaml.YAML()
+    my_dict = dict(author=author, journal_name=journal_name)
 
-    with open('config.yml', 'w') as outfile:
-        yaml.dump(my_dict, outfile)
-
-    #making the directory
-    try:
-        os.makedirs(path)
-    except OSError:
-        print(f"Creating the directory {path} has failed.")
-    else:
-        print(f"Successfully created {author}'s journal at {path}")
-
-    os.chdir(path)
-    add_page()
+    save_config("config.yml", my_dict)
+    init_folder(journal_name)
 
 def add_content(title):
-    timestamp = str(datetime.now())
     with open(title, 'a') as entry:
         writing = questionary.text("What are you grateful for?").ask()
         prettier_writing = textwrap.fill(writing) + "\n"
         entry.write(prettier_writing)
-    git('add', title)
-    git('commit', '-m', timestamp + ' make update to daily entry')
-    git('push')
-        
+
 #flip to the right page
 def add_page():
     entry_name = str(datetime.today().strftime('%Y-%m-%d'))+ ".txt"
     open(entry_name, 'x')
     print("Created Entry" + entry_name)
     add_content(entry_name)
-    
+
 #open the journal
 def open_journal():
-    today_entry = str(datetime.today().strftime('%Y-%m-%d')) + ".txt"
+    entry_name = str(datetime.today().strftime('%Y-%m-%d')) + ".txt"
 
-    os.chdir(path)
+    os.chdir(journal_name)
     journal_list = os.listdir()
 
     #looping through the list of journals to check if there is an entry for today
     if not journal_list:
         add_page()
     for journal in journal_list:
-        if fnmatch.fnmatch(journal, today_entry):
-            add_content(today_entry)
+        if fnmatch.fnmatch(journal, entry_name):
+            add_content(entry_name)
         else:
             add_page()
 
 # flip through past pages
 def read_entries():
-    os.chdir(path)
+    global journal_name
+    os.chdir(journal_name)
     journal_list = os.listdir()
 
     question = [{
@@ -86,26 +107,53 @@ def read_entries():
     with open(entry, 'r') as e:
         print(e.read())
 
-def main():
-    global path,author
-    greeting = "Gratitude Journal"
-    with colors['LIGHT_SEA_GREEN']:
-        print(Figlet(font='slant').renderText(greeting))
-    choice = questionary.select(
-    "What would you like to do",
-    choices=[
-        'Journal',
-        'Read Entries',
-        'Quit'
-    ]).ask()
-    if choice == 'Journal':
-        if path == "":
-            create_journal()
-        else:
-            open_journal()
-    elif choice == 'Read Entries':
-        read_entries()
-    elif choice == 'Quit':
-        print("Goodbye, have a lovely day!")
+# Code credit to harsh183
+def display_push_progress():
+    # Note: Actually getting progress from git is surprisingly non-trivial
+    # So I've hard coded an example for now, but in real code, emit things as they happen
+    with Progress() as progress:
+        push_progress = progress.add_task("Pushing files :partying_face:")
 
-main()
+        for i in range(100):
+            progress.update(push_progress, advance=1)
+            time.sleep(0.05)
+
+class GJournal(cli.Application):
+    VERSION = "0.0"
+
+    push = cli.Flag(['p', 'push'], help="Commits and pushes the added files as well")
+
+    def main(self):
+        global journal_name,author,push
+
+        load_config("config.yml")
+        print_banner("Gratitude Journal")
+    
+        choice = questionary.select(
+        "What would you like to do",
+        choices=[
+            'Journal',
+            'Read Entries',
+            'Quit'
+        ]).ask()
+        if choice == 'Journal':
+            if journal_name == "":
+                create_journal()
+            else:
+                open_journal()
+        elif choice == 'Read Entries':
+            read_entries()
+        elif choice == 'Quit':
+            print("Goodbye, have a lovely day!")
+
+        timestamp = str(datetime.now())
+        entry_name = str(datetime.today().strftime('%Y-%m-%d')) + ".txt"
+
+        if self.push:
+            git('add', entry_name)
+            git('commit', '-m', timestamp + ' make update to daily entry')
+            git('push')
+            display_push_progress()
+
+if __name__ == "__main__":
+    GJournal()
